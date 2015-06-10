e87f237dd3a005f92950d9bf002b39a7f5e35ee7
Peter Havens <peter.havens@gmail.com>
2015-06-10 10:08:21 -0600

    Add RDoc comments to #xtap method.

--- lib/xtap.rb
+++ lib/xtap.rb
@@ -1,6 +1,17 @@
 require 'xtap/version'
 
 class Object
+  # Yields self to its block, and then returns the value of calling its block.
+  # The purpose of this method is similar to Object#tap, but is used to
+  # continue a method chain after transforming a self into something new.
+  #
+  # def transform(s)
+  #   'bar'
+  # end
+  #
+  # 'foo'
+  #   .xtap { |s| transform(s) }
+  #   .gsub(/bar/, 'baz')
   def xtap
     yield self
   end
